004cf28c5ed0414319d5d6b0c5e2a0d8c37d6c08
rauulmedrano <rauulmedrano@gmail.com>
2024-05-14 07:08:38 +0200

    Create NewCategoriesController.java
    Ventana modal donde se añaden las categorías

---
+++ JavaFX11_ExpenseTrack/src/javafxmlapplication/NewCategoriesController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxmlapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author RMEDLLO
+ */
+public class NewCategoriesController implements Initializable {
+
+    
+    @FXML
+    private Button bCancel;
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    
+    
+    
+    @FXML
+    public void closeWindow() {
+        Stage stage = (Stage) bCancel.getScene().getWindow();
+        stage.close();
+    }
+
+    
+    
+}
